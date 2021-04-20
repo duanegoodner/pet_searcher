4b3af1b9aefa316dd4e69b7fd9bd485e0b9703ca
Duane Goodner <dmgoodner@gmail.com>
2021-04-20 17:21:41 +0800

    change AppUserService to depend on app's FirebaseAuth instance

--- lib/screens/user_home_screen.dart
+++ lib/screens/user_home_screen.dart
@@ -1,18 +1,24 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:pet_matcher/screens/landing_screen.dart';
 
 import '../models/app_user.dart';
-import '../services/app_user_service.dart';
+import '../services/firebase_auth_service.dart';
 
-class UserHomeScreen extends StatelessWidget {
+class UserHomeScreen extends StatefulWidget {
   static const routeName = 'userHomeScreen';
+
+  UserHomeScreen({Key key}) : super(key: key);
+
+  @override
+  _UserHomeScreenState createState() => _UserHomeScreenState();
+}
+
+class _UserHomeScreenState extends State<UserHomeScreen> {
+  final _firebaseAuth = FirebaseAuthService();
 
   @override
   Widget build(BuildContext context) {
-    Stream<DocumentSnapshot> appUserDataStream =
-        Provider.of<AppUserService>(context).userDataStream;
+    final AppUser appUser = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       backgroundColor: Colors.blue[300],
       appBar: AppBar(
@@ -24,45 +30,32 @@ class UserHomeScreen extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.logout),
             tooltip: 'Log Out',
-            onPressed: () {
-              _logout(context);
-            },
+            onPressed: _logout,
           ),
         ],
       ),
       body: SafeArea(
         child: Center(
-          child: StreamBuilder(
-            stream: appUserDataStream,
-            builder: (context, snapshot) {
-              if (snapshot.data == null) {
-                return CircularProgressIndicator();
-              } else {
-                AppUser currentUser = AppUser.fromJSON(snapshot.data.data());
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('User Info:'),
-                    Text(
-                        'Name: ${currentUser?.firstName} ${currentUser?.lastName}'),
-                    Text('email: ${currentUser?.email}'),
-                    Text('User Type: ${currentUser?.role}'),
-                    Text('City: ${currentUser?.city}'),
-                    Text('State: ${currentUser?.state}'),
-                    Text('User Zip Code: ${currentUser?.zipCode.toString()}'),
-                  ],
-                );
-              }
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('User Info:'),
+              Text('Name: ${appUser.firstName} ${appUser.lastName}'),
+              Text('email: ${appUser.email}'),
+              Text('User Type: ${appUser.role}'),
+              Text('City: ${appUser.city}'),
+              Text('State: ${appUser.state}'),
+              Text('User Zip Code: ${appUser.zipCode.toString()}'),
+            ],
           ),
         ),
       ),
     );
   }
 
-  void _logout(BuildContext context) async {
-    await Provider.of<fb_auth.FirebaseAuth>(context, listen: false).signOut();
+  void _logout() async {
+    await _firebaseAuth.firebaseSignOut();
     Navigator.of(context).popUntil((route) => route.isFirst);
   }
 }
