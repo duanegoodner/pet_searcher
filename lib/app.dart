3e8789780a35f5fc165b8f5b06280f9b68a352d9
ecapobianco <43688376+ecapobianco@users.noreply.github.com>
2021-04-21 02:29:18 -0500

    Home screens (#18)
    * Created drawer; added dependency for font package
    
    * Start of the layout of admin home page; cleaned up admin_drawer code
    
    * Create news.json
    
    Co-authored-by: duanegoodner <55758382+duanegoodner@users.noreply.github.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,139 +1,32 @@
-import 'package:pet_matcher/screens/user_home_screen.dart';
-import 'package:pet_matcher/services/app_user_service.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
-import 'package:cloud_firestore/cloud_firestore.dart' as cf;
 import 'package:flutter/material.dart';
+import 'package:pet_matcher/screens/user_home_screen.dart';
+import 'screens/add_pet_screen.dart';
 import 'screens/landing_screen.dart';
-import 'screens/test_screen.dart';
-import 'navigation/routes.dart';
+import 'screens/login_screen.dart';
+import 'screens/account_setup_screen.dart';
+import 'screens/admin_home_screen.dart';
 
 class PetMatcherApp extends StatelessWidget {
-  // final Future<FirebaseApp> _initializeFirebase = Firebase.initializeApp();
+  static final routes = {
+    LandingScreen.routeName: (context) => LandingScreen(),
+    LoginScreen.routeName: (context) => LoginScreen(),
+    AccountSetupScreen.routeName: (context) => AccountSetupScreen(),
+    AddPetScreen.routeName: (context) => AddPetScreen(),
+    UserHomeScreen.routeName: (context) => UserHomeScreen(),
+    AdminHomeScreen.routeName: (context) => AdminHomeScreen(),
+  };
 
   @override
   Widget build(BuildContext context) {
-    return FirebaseServiceProviders(
-      child: AppUserServiceProvider(
-        child: MaterialApp(
-          title: 'Pet Matcher',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: StartupScreenSelector(),
-          routes: RouteNames.routes,
-          // initialRoute: LandingScreen.routeName,
-        ),
+    return MaterialApp(
+      title: 'Pet Matcher',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class FirebaseServiceProviders extends StatelessWidget {
-  final Widget child;
-
-  const FirebaseServiceProviders({Key key, this.child}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      Provider(
-        create: (_) => fb_auth.FirebaseAuth.instance,
-      ),
-      Provider(
-        create: (_) => cf.FirebaseFirestore.instance,
-      ),
-    ], child: child);
-  }
-}
-
-class AppUserServiceProvider extends StatelessWidget {
-  final Widget child;
-
-  const AppUserServiceProvider({Key key, this.child}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final firebaseAuth = Provider.of<fb_auth.FirebaseAuth>(context);
-    return Provider(
-      create: (context) => AppUserService(firebaseAuth: firebaseAuth),
-      child: child,
-    );
-  }
-}
-
-class ExistingLoginChecker extends StatelessWidget {
-  const ExistingLoginChecker({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Provider.of<fb_auth.FirebaseAuth>(context).authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          if (snapshot.data == null) {
-            return LandingScreen();
-          }
-          // change to user role checker
-          return UserHomeScreen();
-        } else {
-          return Scaffold(
-            backgroundColor: Colors.blue[300],
-            body: Center(
-              child: Column(
-                children: [
-                  Text(
-                    'Wating for Firebase Connection',
-                    style: TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                    ),
-                  ),
-                  CircularProgressIndicator(),
-                ],
-              ),
-            ),
-          );
-        }
-      },
-    );
-  }
-}
-
-class StartupScreenSelector extends StatelessWidget {
-  const StartupScreenSelector({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Provider.of<fb_auth.FirebaseAuth>(context).authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          if (snapshot.data == null) {
-            return LandingScreen();
-          }
-          return UserHomeScreen();
-        } else {
-          return Scaffold(
-            backgroundColor: Colors.blue[300],
-            body: Center(
-              child: Column(
-                children: [
-                  Text(
-                    'Wating for Firebase Connection',
-                    style: TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                    ),
-                  ),
-                  CircularProgressIndicator(),
-                ],
-              ),
-            ),
-          );
-        }
-      },
+      routes: routes,
+      //initialRoute: LandingScreen.routeName,
+      //initialRoute: AddPetScreen.routeName,
+      initialRoute: AdminHomeScreen.routeName,
     );
   }
 }
