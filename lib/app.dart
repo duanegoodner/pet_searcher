340c3121d68877d7f433de71c63cf0b989cc4812
ecapobianco <43688376+ecapobianco@users.noreply.github.com>
2021-04-24 01:56:55 -0500

    New home screens (#23)
    * Home screens (#18)
    
    * Created drawer; added dependency for font package
    
    * Start of the layout of admin home page; cleaned up admin_drawer code
    
    * Create news.json
    
    Co-authored-by: duanegoodner <55758382+duanegoodner@users.noreply.github.com>
    
    * Basic FirebasAuth Provider set up with TestScreen() using Provider.of<FirebaseAuth>
    
    * add StartupScreenSelector widget that determines initial page based on FirebaseUser login state
    
    * change AppUserService to depend on app's FirebaseAuth instance
    
    * Add service providers as top level apps. Stream user data to User Homescreen.
    
    * Send pre-signed-in user to correct home (admin or user). Connect logout callback to admin drawer.
    
    * Add logic to direct user to correct screen upon login
    
    * Home screens (#18)
    
    * Created drawer; added dependency for font package
    
    * Start of the layout of admin home page; cleaned up admin_drawer code
    
    * Create news.json
    
    Co-authored-by: duanegoodner <55758382+duanegoodner@users.noreply.github.com>
    
    * Basic FirebasAuth Provider set up with TestScreen() using Provider.of<FirebaseAuth>
    
    * change AppUserService to depend on app's FirebaseAuth instance
    
    * Add service providers as top level apps. Stream user data to User Homescreen.
    
    * Home screens (#18)
    
    * Created drawer; added dependency for font package
    
    * Start of the layout of admin home page; cleaned up admin_drawer code
    
    * Create news.json
    
    Co-authored-by: duanegoodner <55758382+duanegoodner@users.noreply.github.com>
    
    * Basic FirebasAuth Provider set up with TestScreen() using Provider.of<FirebaseAuth>
    
    * change AppUserService to depend on app's FirebaseAuth instance
    
    * Add service providers as top level apps. Stream user data to User Homescreen.
    
    * Send pre-signed-in user to correct home (admin or user). Connect logout callback to admin drawer.
    
    * Add logic to direct user to correct screen upon login
    
    * Broke my github, made new branch. Played around with more formatting on screen.
    
    * Admin home screen mostly finished. Needs some refactoring and stuff added in.
    
    * Cute animal pictures
    
    * fix typo that happened during Github merge conflict resolution.
    
    Co-authored-by: duanegoodner <55758382+duanegoodner@users.noreply.github.com>
    Co-authored-by: Duane Goodner <dmgoodner@gmail.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
-//import 'package:pet_matcher/screens/admin_home_screen.dart';
+import 'package:pet_matcher/screens/add_pet_screen.dart';
+import 'package:pet_matcher/screens/admin_home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
 import 'package:flutter/material.dart';
--- lib/screens/admin_home_screen.dart
+++ lib/screens/admin_home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-//import 'package:flutter/services.dart';
-//import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:convert';
 import 'package:pet_matcher/widgets/admin_drawer.dart';
 
 class AdminHomeScreen extends StatefulWidget {
@@ -14,9 +14,9 @@ class AdminHomeScreen extends StatefulWidget {
 class _AdminHomeScreenState extends State<AdminHomeScreen> {
   //Map<String, dynamic> newsData;
   final String heading = "Happy National Pet Day!";
-  final String body = "Show us your best pictures of your furry friends!\n"
-      "Don't currently have a fluffy friend to love?\n"
-      "Browse through our available pets and arrange\n "
+  final String body = "Show us your best pictures of your furry friends! "
+      "Don't currently have a fluffy friend to love? "
+      "Browse through our available pets and arrange "
       "a date today!";
 
   @override
@@ -27,51 +27,167 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
         title: Text('Admin'),
         backgroundColor: Colors.blue[300],
       ),
-      drawer: AdminDrawer(),
-      backgroundColor: Colors.blue[300],
-      body: Center(
-        child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Container(
-                height: 30,
-                decoration: BoxDecoration(color: Colors.blue[300])
-              ),
-              recentNewsItemListTile(context),
-            ],
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            headingText('News Feed:'),
+            Center(
+              child: newsCard(),
+            ),
+            headingText('Manage Inventory:'),
+            inventoryCard(),
+            headingText('Featured Animals:'),
+            featuredAnimals(),
+          ],
         ),
       ),
     );
   }
 
-  Widget recentNewsItemListTile(BuildContext context) {
-    return ListTile(
-      title: Text(
-        '$heading',
-        textAlign: TextAlign.center,
+  Widget headingText(String heading) {
+    return Container(
+      margin: EdgeInsets.only(top: 20.0),
+      child: Text(
+        '${heading}',
         style: TextStyle(
           fontSize: 26,
+          color: Colors.white,
         ),
       ),
-      subtitle: Column(
-        children: [
-          Text('$body',textAlign: TextAlign.center, style: TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {},
-            ),
-          ],
-        )],
-      ), 
     );
   }
 
+  Widget newsCard() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      width: 275,
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                '${heading}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Text(
+                '${body}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    //Do we want a popup window? New page?
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget inventoryCard() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      width: 275,
+      height: 265,
+      child: Card(
+        color: Colors.white,
+        elevation: 0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Card(
+                child: Wrap(children: <Widget>[
+              Image.asset('assets/images/frenchie_in_costume.jpg',
+                  height: 200, width: 275, fit: BoxFit.fitWidth),
+            ])),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    //Animal inventory page?
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget featuredAnimals() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      height: 285,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          featuredAnimalCard(),
+          featuredAnimalCard(),
+          featuredAnimalCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget featuredAnimalCard() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      width: 160,
+      child: Card(
+        color: Colors.white,
+        elevation: 0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Card(
+                child: Wrap(children: <Widget>[
+              Image.asset('assets/images/puppy.jpg',
+                  height: 165, width: 275, fit: BoxFit.fitHeight),
+            ])),
+            Row(
+              children: <Widget>[],
+            ),
+            Row(
+              children: <Widget>[
+                animalCardText('Adult male', 14.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      //Animal inventory page?
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget animalCardText(String animalText, double size) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text('${animalText}', style: TextStyle(fontSize: size)),
+    );
+  }
 }
