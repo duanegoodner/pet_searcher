b7af891963c888798cd1894969587b1f57f50e8a
Traci Goreham <traci.goreham@gmail.com>
2021-04-30 19:25:18 -0500

    news feed fixes, added sharing, added logo to drawers

--- lib/widgets/user_drawer.dart
+++ lib/widgets/user_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
+//import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pet_matcher/screens/news_screen.dart';
 import 'package:pet_matcher/locator.dart';
@@ -17,22 +17,30 @@ class UserDrawer extends StatelessWidget {
         ),
         child: Column(
           children: <Widget>[
-            Container(
-              height: 120,
-              width: double.infinity,
-              padding:
-                  EdgeInsets.only(top: 50, left: 15, right: 20, bottom: 20),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[700]),
+              child: Column(
+                children: [
+                  Container(
+                    height: 90,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset('assets/images/paw_logo.png'),
+                  ),
+                  Container(
+                    padding:
+                    EdgeInsets.only(top: 5, left: 15, right: 15, bottom: 10),
+                    child: Text(
+                      'Menu',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ]
               ),
             ),
-            SizedBox(height: 10),
             buildListTile(
               title: 'Home',
               icon: Icons.home,
@@ -100,7 +108,7 @@ class UserDrawer extends StatelessWidget {
   }
 
   void pushNewsScreen(BuildContext context) {
-    Navigator.of(context).pushNamed(NewsScreen.routeName);
+    Navigator.of(context).pushNamed(NewsScreen.routeName, arguments: 'user');
   }
 
   /*void pushFavoritesScreen(BuildContext context) {
