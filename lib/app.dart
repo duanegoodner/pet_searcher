8d04dfaa5c1fb0d85e467dc27d41be8abfcd1e61
Duane Goodner <dmgoodner@gmail.com>
2021-05-01 17:05:24 +0800

    remove unused imports and widget from app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,10 @@
 import 'package:pet_matcher/navigation/routes.dart';
 import 'package:pet_matcher/navigation/startup_screen_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:pet_matcher/screens/admin_home_screen.dart';
-import 'package:pet_matcher/screens/user_home_screen.dart';
-import 'package:pet_matcher/screens/landing_screen.dart';
-import 'package:pet_matcher/widgets/standard_loading_screen.dart';
 import 'package:provider/provider.dart';
 //import 'package:pet_matcher/screens/news_screen.dart';
 import 'package:pet_matcher/services/app_user_service.dart';
 import 'package:pet_matcher/locator.dart';
-import 'package:pet_matcher/models/app_user.dart';
 
 class PetMatcherApp extends StatelessWidget {
   @override
@@ -27,18 +22,5 @@ class PetMatcherApp extends StatelessWidget {
         initialRoute: StartUpScreenController.routeName,
       ),
     );
-  }
-
-  Widget startupScreenSelector(BuildContext context) {
-    AppUser appUser = Provider.of<AppUser>(context);
-
-    if (appUser == null) {
-      return standardLoadingScreen();
-    } else if (appUser.role == 'admin') {
-      return AdminHomeScreen();
-    } else if (appUser.role == 'publicUser') {
-      return UserHomeScreen();
-    }
-    return LandingScreen();
   }
 }
