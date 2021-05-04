72f53575fa5c2717f584c9badda9ca611b41a801
gorehamt <32149305+gorehamt@users.noreply.github.com>
2021-05-03 20:57:51 -0500

    Update initial route (#36)
    * remove warnings and change initial route back
    
    * update initial route
    
    Co-authored-by: Traci Goreham <traci.goreham@gmail.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,7 @@
 import 'package:pet_matcher/navigation/routes.dart';
 import 'package:pet_matcher/navigation/startup_screen_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:pet_matcher/screens/animal_inventory_screen.dart';
 import 'package:provider/provider.dart';
-//import 'package:pet_matcher/screens/news_screen.dart';
 import 'package:pet_matcher/services/app_user_service.dart';
 import 'package:pet_matcher/locator.dart';
 
@@ -18,10 +16,8 @@ class PetMatcherApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         // home: startupScreenSelector(context),
-        //home: NewsScreen(),
         routes: RouteNames.routes,
-        // initialRoute: StartUpScreenController.routeName,
-        initialRoute: AnimalInventoryScreen.routeName,
+        initialRoute: StartUpScreenController.routeName,
       ),
     );
   }
