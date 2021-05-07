dbde2258b4bedf36f80693253546e56ef93514e3
Duane Goodner <dmgoodner@gmail.com>
2021-05-07 20:45:47 +0800

    finish merge

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,8 @@
 import 'package:pet_matcher/navigation/routes.dart';
 import 'package:pet_matcher/navigation/startup_screen_controller.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-//import 'package:pet_matcher/screens/add_news_item_screen.dart';
-=======
 import 'package:pet_matcher/models/animal.dart';
 import 'package:pet_matcher/services/animal_service.dart';
->>>>>>> search_popup
 import 'package:provider/provider.dart';
 import 'package:pet_matcher/services/app_user_service.dart';
 import 'package:pet_matcher/locator.dart';
