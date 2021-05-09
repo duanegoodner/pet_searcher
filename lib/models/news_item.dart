82d400c6023eae0d1f8fea5be80bc6a23858f0ff
gorehamt <32149305+gorehamt@users.noreply.github.com>
2021-05-08 20:21:09 -0500

    news feed editing (#41)
    Co-authored-by: Traci Goreham <traci.goreham@gmail.com>

--- lib/models/news_item.dart
+++ lib/models/news_item.dart
@@ -1,13 +1,15 @@
 class NewsItem {
+  String docID;
   String imageUrl;
   String title;
   String body;
   DateTime date;
 
-  NewsItem({this.imageUrl, this.title, this.body, this.date});
+  NewsItem({this.docID, this.imageUrl, this.title, this.body, this.date});
 
   factory NewsItem.fromJSON(Map<String, dynamic> json) {
     return NewsItem(
+      docID: json['docID'],
       imageUrl: json['imageUrl'],
       title: json['title'],
       body: json['body'],
@@ -16,6 +18,7 @@ class NewsItem {
   }
 
   NewsItem.fromMap(Map<String, dynamic> info) {
+    this.docID = info['docID'];
     this.imageUrl = info['imageUrl'];
     this.title = info['title'];
     this.body = info['body'];
