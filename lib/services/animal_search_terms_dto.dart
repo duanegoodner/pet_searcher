acf4e7e349dddb299988b288192227a09e4d934b
Duane Goodner <dmgoodner@gmail.com>
2021-05-16 18:42:27 +0800

    Add "search by date" functionality

--- lib/services/animal_search_terms_dto.dart
+++ lib/services/animal_search_terms_dto.dart
@@ -1,26 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 class AnimalSearchTermsDTO {
   String type;
   String breed;
   String gender;
   List<String> disposition;
+  DateTimeRange dateAdded;
 
   AnimalSearchTermsDTO({
     this.type,
     this.breed,
     this.gender,
     this.disposition,
+    this.dateAdded,
   });
 
   AnimalSearchTermsDTO.initial()
       : type = null,
         breed = null,
         gender = null,
-        disposition = [];
+        disposition = [],
+        dateAdded = DateTimeRange(
+          start: DateTime.utc(2018),
+          end: DateTime.now(),
+        );
 
   Map<String, dynamic> toJson() => {
         'type': type,
         'breed:': breed,
         'gender': gender,
         'disposition': disposition,
+        'dateAdded': dateAdded,
       };
+
+  String get formattedDate =>
+      '${DateFormat.yMMMd().format(dateAdded.start)} - ${DateFormat.yMMMd().format(dateAdded.end)}';
 }
